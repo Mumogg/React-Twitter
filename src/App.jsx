c89f5d9a40424a7192d07adca48dfe184a1546de
Sebastian Murillo <104529023+Mumogg@users.noreply.github.com>
2024-03-07 12:48:13 -0600

    class 581

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import Post from "./components/Post";
 function App(){
-  return <Post />;
+  return (
+  <main>
+    <Post author="Sebastian" body="viva la liga"/>
+    <Post author="Samuel" body="viva la roma"/>
+  </main>
+  );
 }
 
 export default App;
